aba60662cb19eb920bd6278c4a4c26156527b1f9
DominikMikulic <dmikulic@tvz.hr>
2021-05-25 21:25:11 +0100

    članovi dominik mikulic

--- clanovi.php
+++ clanovi.php
@@ -13,6 +13,103 @@
     <title>Veleumovi TVZ-a</title>
 </head>
 <body>
-   
+<div class="container-fluid clanovi">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-lg-12 title">
+				<h1>Članovi tima Veleumovi TVZ-a </h1>
+            </div>
+		</div>
+    </div>
+</div>
+
+
+
+<div class="container-fluid abt">
+    <div class="container">
+    <div class="row profilna">
+      <div class="col-12 col-lg-6 spusti_sliku">
+        <img src="mikulic.png">
+        </div>
+          <div class="col-12 col-lg-6 info">
+           <h3>Dominik Mikulić</h3>
+          <p class="spustip">Dominik Mikulić student je TVZ-a koji je rođen 1998. godine. Kada ne rješava stvari za faks, slobodno vrijeme provodi družeći se s prijateljima i baveći se raznim sportovima./p>
+          <p class="spustip">TVZ je upisao jer je shvatio da FER nije bio pravi odabir te je znao da je TVZ izvrsna podloga za daljnje napredovanje. Upisao je smjer Elektroničko poslovanje iako su mu sva tri smjera bila podjednako privlačna.
+            Odmah pri završetku fakulteta nada se pronalasku posla u IT sektoru te radu upravo u struci koju i studira.</p>
+          <div class="social">
+            <h2><a href=""><i class="fab fa-facebook" style="color: blue;"></i></a>
+                <a href=""><i class="fab fa-instagram"  style="color: red;"></i></a>
+                <a href=""><i class="fab fa-twitter"  style="color: cyan;"></i></a></h2>
+          </div>
+      </div>
+    </div>
+    </div>
+    </div>
+
+    <div class="container-fluid abt obojaj dole">
+        <div class="container">
+        <div class="row">
+
+              <div class="col-12 col-lg-6 info">
+
+               <h3>Ante Pišković</h3>
+              <p class="spusti_odlomak">Ante Pišković je mladi momak koji studira informatiku na TVZ-u.
+                Rođen je 2000 godine u lipnju, te će uskoro napuniti 21 godinu. Radi studentski posao kao agent u telekomunikacijskoj kompaniji.
+                </p>
+              <p class="spustip">Zbog vremenskih ograničenja se bavi samo polu profesionalno 3d projektiranjem u slobodno vrijeme. Njegovi hobiji su 3d printanje figurica/skulptura, čitanje knjiga, plivanje i mnogo drugih. Razlog zašto je odrabrao TVZ je bila njegova ljubav prema programiranju.
+                Ta se ljubav na TVZ-u pretvorila u nešto više, u ljubav prema svemu vezano za tehnologiju.</p>
+              <div class="social">
+                <h2><a href=""><i class="fab fa-facebook" style="color: blue;"></i></a>
+                    <a href=""><i class="fab fa-instagram"  style="color: red;"></i></a>
+                    <a href=""><i class="fab fa-twitter"  style="color: cyan;"></i></a></h2>
+              </div>
+        </div>
+          <div class="col-12 col-lg-6 spusti_sliku">
+            <img src="piskovic.png">
+            </div>
+        </div>
+        </div>
+        </div>
+
+        <div class="container-fluid abt">
+            <div class="container">
+            <div class="row">
+              <div class="col-12 col-lg-6 spusti_sliku">
+                <img src="prtenjaca.png">
+                </div>
+                  <div class="col-12 col-lg-6 info">
+                   <h3>Matej Prtenjača</h3>
+                  <p class="spustip">Matej Prtenjača je 23-godišnjak rođen 1998. godine. Bavi se MMA-om, studira i radi kao agent u korisničkoj službi. </p>
+                  <p class="spustip">Slobodno vrijeme provodi trenirajući, družeći se s prijateljima, gledanjem serija i filmova te igranjem videoigara. TVZ informatiku  je upisao da po preporuci više ljudi nakon što je 3 godine studirao elektrotehniku također na TVZ-u.</p>
+                  <div class="social">
+                    <h2><a href=""><i class="fab fa-facebook" style="color: blue;"></i></a>
+                        <a href=""><i class="fab fa-instagram"  style="color: red;"></i></a>
+                        <a href=""><i class="fab fa-twitter"  style="color: cyan;"></i></a></h2>
+                  </div>
+              </div>
+            </div>
+            </div>
+            </div>
+
+            <div class="container-fluid abt obojaj dole">
+                <div class="container">
+                <div class="row">
+           
+                      <div class="col-12 col-lg-6 info">
+                       <h3>Domagoj Paradžiković</h3>
+                      <p class="spustip">Domagoj Paradžiković je 21-godišnji student TVZ-a. Rođen 2000 godine u Osijeku. Radi kao agent u korisničkoj podršci korisnicima.</p>
+                      <p class="spustip">U slobodno vrijeme se druži s ekipom. Svira i zabavlja se na sve moguće načine. Tehničko Veleučilište je upisao jer je u tom fakultetu vidio mogućnost za napredovanjem i razvojem</p>
+                      <div class="social">
+                        <h2><a href=""><i class="fab fa-facebook" style="color: blue;"></i></a>
+                            <a href=""><i class="fab fa-instagram"  style="color: red;"></i></a>
+                            <a href=""><i class="fab fa-twitter"  style="color: cyan;"></i></a></h2>
+                      </div>
+                  </div>
+                  <div class="col-12 col-lg-6 spusti_sliku">
+                    <img src="paradzikovic.jpg">
+                    </div>
+                </div>
+                </div>
+                </div>
 </body>
 </html>
